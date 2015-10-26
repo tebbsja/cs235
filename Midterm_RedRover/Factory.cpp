ebf67a09392d4ebe5d15173caeeb884e7c58c7a0
Austin Tebbs <stinden1@yahoo.com>
2015-10-26 11:25:26 -0600

    Oct 26 2015

---
+++ Midterm_RedRover/Factory.cpp
@@ -0,0 +1,22 @@
+#include "Factory.h"
+//You may add #include statments here
+using namespace std;
+
+/*
+Unlike all other documents provided, you may modify this document slightly (but do not change its name)
+*/
+//=======================================================================================
+/*
+createRedRover()
+
+Creates and returns an object whose class extends RedRoverInterface.
+This should be an object of a class you have created.
+
+Example: If you made a class called "RedRover", you might say, "return new RedRover();".
+*/
+RedRoverInterface* Factory::createRedRover()
+{
+	return NULL;//Modify this line
+
+}
+//================================
